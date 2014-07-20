56d85be9b745fbddcebfeff3a5f78be3669c9397
Ethan Resnick <ethan.resnick@gmail.com>
2014-07-19 23:37:05 -0400

    Make the firewall's 401 and 403 handling more generic
    Now it calls a callback for each, rather than trying to render a
    template or return json. lets you set things in the response like the
    www-authenticate header, if you want.

--- test/simple-firewall.js
+++ test/simple-firewall.js
@@ -1,11 +1,9 @@
 var firewall = require('../index')
   , expect = require('chai').expect
   , express = require('express')
-  , sinonChai = require("sinon-chai")
   , sinon = require('sinon')
-  , Q = require("Q")
   , emitter = new (require('events').EventEmitter)
-  , app = express(); 
+  , Q = require("Q");
 
 var routes = [
   {path:"/members", access: "MEMBER"},
@@ -21,107 +19,75 @@ var dummyUsers = [
   {hasRole: sinon.spy(function(role) { return role === "MEMBER"; }), isApproved: true}
 ];
 
+//req.user is set directly on these, rather than it being set
+//through the adapter, b/c the adapters have their own unit tests.
 var requests = {
-  invalidSession: {
-    url: "/members", 
-    method: "get", 
-    session: {"user_id": "invalid"}
-  },
   sessionUser0: {
     url: "/members", 
     method: "get", 
-    session: {"user_id": "0"}
+    user: dummyUsers[0]
   },
   unspecifiedPolicyUser1: {
     url: "/unspecifiedPolicy", 
     method: "get", 
-    session: {"user_id": "1"}
+    user: dummyUsers[1]
   },  
   unspecifiedPolicyNoUser: {
     url: "/unspecifiedPolicy", 
-    method: "get",
-    session: {}
+    method: "get"
   },
   publicNoUser: {
     url: '/public',
-    method: 'post',
-    session: {}
+    method: 'post'
   },
   publicWithUser: {
     url: '/public',
     method: 'post',
-    session: {"user_id": "0"}
+    user: dummyUsers[0]
   },
   memberNoUser: {
     url: '/members',
-    method: "get",
-    session: {}
+    method: "get"
   },
   memberAuthorizedAccess: {
     url: "/members", 
     method: "get", 
-    session: {"user_id": "3"}
+    user: dummyUsers[3]
   },
   memberAccessNoRole: {
     url: "/members", 
     method: "get", 
-    session: {"user_id": "1"}
+    user: dummyUsers[1]
   },
   memberAccessUnapprovedUser: {
     url: "/members", 
     method: "get", 
-    session: {"user_id": "2"}
+    user: dummyUsers[2]
   },
   requiresUser: {
     url: "/user",
     method: "get",
-    session: {"user_id": "3"}    
+    user: dummyUsers[3]
   },
   requiresUserNoUser: {
     url: "/user",
     method: "get",
-    session: {"user_id": "3"}    
+    user: dummyUsers[3]
   },
   requiresUserUnapprovedUser: {
     url: "/user",
     method: "get",
-    session: {"user_id": "0"}
+    user: dummyUsers[0]
   }
 };
 
-var resSpy = {
-  'status': sinon.spy(function(res, req, next) { }), 
-  'render': sinon.spy(function(template) { 
-    //we need to be able to run a callback post render in our tests to see
-    //that the right thing happened, but express doesn't give us an easy way 
-    //to do that, so we're having our render stub emit an event.
-    emitter.emit('renderComplete'); 
-    emitter.emit('sendComplete');
-  }),
-  'json': sinon.spy(function(status, obj) {
-    emitter.emit('sendComplete');
-  })
-};
+var resSpy = {'status': sinon.spy(function(res, req, next) { })}
+  , firewallAdapter = sinon.spy(function(req, res, next){ next(); })
+  , routeHandler = sinon.spy(function(req, res, next) { emitter.emit('done'); })
+  , unauthenticatedHandler = sinon.spy(function(req, res) { emitter.emit('done'); })
+  , unauthorizedHandler = sinon.spy(function(req, res) { emitter.emit('done'); });
 
-var routeHandler = sinon.spy(function(req, res, next) {
-  next(); //so we get to the done callback
-});
-
-var userGetter = function(userId) {
-  if(userId == "invalid") {
-    return Q.Promise(function(resolve, reject, notify) {
-      reject(new Error("Invalid user!"));
-    });
-  }
-  else {
-    return Q.Promise(function(resolve, reject, notify) {
-      resolve(dummyUsers[userId]);
-    });
-  }
-};
-
-var adapter = firewall.adapters.sessionBased(userGetter);
-var firewallRouter = firewall(routes, adapter, "loginTemp", "403Temp");
+var firewallRouter = firewall(routes, firewallAdapter, unauthenticatedHandler, unauthorizedHandler);
 var mainRouter = express.Router();
 mainRouter.use(firewallRouter);
 mainRouter.use(routeHandler);
@@ -141,62 +107,51 @@ describe("the firewall itself", function() {
   //requests, in between each of which we need to call this.
   function reset() {
     resSpy.status.reset();
-    resSpy.render.reset();
+    unauthenticatedHandler.reset();
+    unauthorizedHandler.reset();
     routeHandler.reset();
+    firewallAdapter.reset();
   }
 
-  function expectSuccessPromise(req) {
+  //The express router doesn't let us register a callback to run when the last middleware is done.
+  //So, instead, we have our final handlers trigger events, which we listen to in the fns below.
+  //Note: the Router will let you provide a callback that's run if next() is called from the 
+  //last middleware (https://github.com/visionmedia/express/blob/master/lib/router/index.js#L115), 
+  //but some of our final functions (i.e. the unauthenticated/unauthorizedHandlers) are inten-
+  //tionally called without next as an argument, to make sure the user doesn't accidentally let
+  //the request through the firewall. Hence our EventEmitter test pattern.
+  function expectStatusPromise(req, status, additionalExpectations) {
     return Q.promise(function(resolve, reject) {
       reset();
-      mainRouter(req, resSpy, function(err) {
+      emitter.once('done', function() {
         try {
-          expect(routeHandler.callCount).to.equal(1);
-          expect(resSpy.render.callCount).to.equal(0);
-          resolve();
-        }
-        catch(e) {
-          reject(e);
-        }
-      });
-    });
-  };
+          expect(routeHandler.callCount).to.equal(status === 200 ? 1 : 0);
+          expect(unauthenticatedHandler.callCount).to.equal(status === 401 ? 1 : 0);
+          expect(unauthorizedHandler.callCount).to.equal(status === 403 ? 1 : 0);
 
-  function expectFailurePromise(req, status, expectJSONResponse, additionalExpectations) {
-    return Q.promise(function(resolve, reject) {
-      reset();
-      //test should end in rendering a 403/401 with login page or error, 
-      //hence the emitter pattern.
-      emitter.once('sendComplete', function() {
-        try {
-          expect(routeHandler.called).to.be.false;
-          expect(resSpy[expectJSONResponse ? 'json' : 'render'].callCount).to.equal(1);
-          expect(resSpy.status.calledWith(status)).to.be.true;
-          
-          if(status==403) {
-            expect(resSpy.render.calledWith("403Temp")).to.be.true;
-          }
-          else if(status==401) {
-            expect(resSpy.render.calledWith("loginTemp")).to.be.true;
+          if(status !== 200) {
+            expect(resSpy.status.calledWith(status)).to.be.true;
           }
 
           if(typeof additionalExpectations == 'function') {
             additionalExpectations();
           }
+
           resolve();
         }
         catch(e) {
           reject(e);
         }
       });
-      //trigger req
-      firewallRouter(req, resSpy);
+      //dispatch the request
+      mainRouter(req, resSpy);
     });
   };
 
   function testUserAuthPromise(req, shouldWork) {
     return Q.promise(function(resolve, reject) {
-      routeHandler.reset();
-      var cb = function() {
+      reset();
+      emitter.once('done', function() {
         try {
           expect(routeHandler.callCount).to.equal(shouldWork ? 1 : 0);
           expect(req.user.hasRole.callCount).to.equal(1);
@@ -206,17 +161,8 @@ describe("the firewall itself", function() {
         catch(e) {
           reject(e);
         }
-      };
-
-      //trigger req. if shouldWork, then we're getting out of the 
-      //router and can use the done callback; otherwise we need events.
-      if(shouldWork) {
-        mainRouter(req, resSpy, cb);
-      }
-      else {
-        emitter.once('renderComplete', cb);
-        mainRouter(req, resSpy);
-      }
+      })
+      mainRouter(req, resSpy);
     });
   };
 
@@ -226,20 +172,38 @@ describe("the firewall itself", function() {
     }, promises[0]).then(done, done);
   }
 
+  it("should call the user adapter before anything else", function(done) {
+    expectStatusPromise(requests.publicNoUser, 200, function() {
+      expect(firewallAdapter.calledBefore(routeHandler));
+    }).then(done);
+  })
+
   it("should let anyone access public urls", function(done) {
-    expectSuccessPromise(requests.publicNoUser).then(done);
+    expectStatusPromise(requests.publicNoUser, 200).then(done);
   });
 
   it("should let any user access public urls", function(done) {
-    expectSuccessPromise(requests.publicWithUser).then(done);
-  });
-
-  it("should make routes with no explicit access policy inaccessible with a 403 if a user", function(done) {
-    expectFailurePromise(requests.unspecifiedPolicyUser1, 403).then(done);
+    expectStatusPromise(requests.publicWithUser, 200).then(done);
   });
 
   it("should make routes with no explicit access policy inaccessible with a 401 if no user", function(done) {
-    expectFailurePromise(requests.unspecifiedPolicyNoUser, 401).then(done);
+    expectStatusPromise(requests.unspecifiedPolicyNoUser, 401).then(done);
+  });
+
+  it("should make secure routes inaccessible with a 401 if no user", function(done) {
+      expectStatusPromise(requests.memberNoUser, 401).then(done);
+  });
+
+  it("should allow any user to access routes with an AUTHENTICATED policy", function(done) {
+      promiseSequenceAndDone([
+        expectStatusPromise(requests.requiresUser, 200),
+        expectStatusPromise(requests.requiresUserNoUser, 401),
+        expectStatusPromise(requests.requiresUserUnapprovedUser, 200)
+      ], done);
+  });
+
+  it("should make routes with no explicit access policy inaccessible with a 403 if a user", function(done) {
+    expectStatusPromise(requests.unspecifiedPolicyUser1, 403).then(done);
   });
 
   describe("authenticating the user", function() {
@@ -252,24 +216,10 @@ describe("the firewall itself", function() {
     });
   });
 
-  it("should allow any user to access routes with an AUTHENTICATED policy", function(done) {
-      promiseSequenceAndDone([
-        expectSuccessPromise(requests.requiresUser),
-        expectFailurePromise(requests.requiresUserNoUser, 401),
-        expectSuccessPromise(requests.requiresUserUnapprovedUser)
-      ], done);
-  });
-
   it("should 403 if req.user exists but doesn't have access", function(done) {
     promiseSequenceAndDone([
-      expectFailurePromise(requests.sessionUser0, 403),
-      expectFailurePromise(requests.memberAccessUnapprovedUser, 403)
+      expectStatusPromise(requests.sessionUser0, 403),
+      expectStatusPromise(requests.memberAccessUnapprovedUser, 403)
     ], done);
-  });
-
-  it("should 401, render the login page, and set req.session.goingTo if req.user doesn't exist and is needed", function(done) {
-      expectFailurePromise(requests.memberNoUser, 401, false, function() {
-        expect(requests.memberNoUser.session.goingTo).to.equal('/members');
-      }).then(done);
   });
 });
